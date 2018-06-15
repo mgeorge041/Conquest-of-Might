7ab3bbcd69f5a50fd0f1054f7058597babd0e6d8
mgeorge041 <33108933+mgeorge041@users.noreply.github.com>
2018-06-15 14:53:31 -0700

    Add files via upload

---
+++ CardDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+    public Card card;
+
+    public Text nameText;
+    public Text descText;
+
+    public Image artwork;
+
+    public Text healthText;
+    public Text rangeText;
+    public Text mightText;
+    public Text speedText;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        //nameText.text = card.cardName;
+        //descText.text = card.cardDesc;
+
+        //artwork.sprite = card.artwork;
+
+        //healthText.text = card.health.ToString();
+        //rangeText.text = card.range.ToString();
+        //mightText.text = card.might.ToString();
+        //speedText.text = card.speed.ToString();
+	    	
+	}
+}
---
+++ DropZone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        if (eventData.pointerDrag == null)
+            return;
+
+        //If enters into droppable zone, sets zone to destination
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        d.finalParent = this.transform;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        if (eventData.pointerDrag == null)
+            return;
+
+        //If leaves droppable zone, sets destination to origin
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        d.finalParent = d.initialParent;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
